b9a051789239dfab0c1f156f929fe0a431f5a4b5
johanna <johanna.herpoz@educa.jcyl.es>
2022-05-11 12:19:56 +0200

    11/05/2022 Modificaciones

--- view/vREST.php
+++ view/vREST.php
@@ -9,59 +9,58 @@
                 <div class="container auto">
                     <?php 
                     $opciones = [
-                ['value' => "01", "nombre" => "ALAVA"],
-                ['value' => "02", "nombre" => "ALBACETE"],
-                ['value' => "03", "nombre" => "ALICANTE"],
-                ['value' => "04", "nombre" => "ALMERIA"],
-                ['value' => "33", "nombre" => "ASTURIAS"],
-                ['value' => "05", "nombre" => "AVILA"],
-                ['value' => "06", "nombre" => "BADAJOZ"],
-                ['value' => "08", "nombre" => "BARCELONA"],
-                ['value' => "09", "nombre" => "BURGOS"],
-                ['value' => "10", "nombre" => "CACERES"],
-                ['value' => "11", "nombre" => "CADIZ"],
-                ['value' => "39", "nombre" => "CANTABRIA"],
-                ['value' => "12", "nombre" => "CASTELLON"],
-                ['value' => "51", "nombre" => "CEUTA"],
-                ['value' => "13", "nombre" => "CIUDAD REAL"],
-                ['value' => "14", "nombre" => "CORDOBA"],
-                ['value' => "15", "nombre" => "CORUÑA"],
-                ['value' => "16", "nombre" => "CUENCA"],
-                ['value' => "17", "nombre" => "GIRONA"],
-                ['value' => "18", "nombre" => "GRANADA"],
-                ['value' => "19", "nombre" => "GUADALAJARA"],
-                ['value' => "20", "nombre" => "GUIPUZCOA"],
-                ['value' => "21", "nombre" => "HUELVA"],
-                ['value' => "22", "nombre" => "HUESCA"],
-                ['value' => "07", "nombre" => "ILLES BALEARS"],
-                ['value' => "23", "nombre" => "JAEN"],
-                ['value' => "24", "nombre" => "LEON"],
-                ['value' => "25", "nombre" => "LLEIDA"],
-                ['value' => "27", "nombre" => "LUGO"],
-                ['value' => "28", "nombre" => "MADRID"],
-                ['value' => "29", "nombre" => "MALAGA"],
-                ['value' => "52", "nombre" => "MELILLA"],
-                ['value' => "30", "nombre" => "MURCIA"],
-                ['value' => "31", "nombre" => "NAVARRA"],
-                ['value' => "32", "nombre" => "OURENSE"],
-                ['value' => "34", "nombre" => "PALENCIA"],
-                ['value' => "35", "nombre" => "LAS PALMAS"],
-                ['value' => "36", "nombre" => "PONTEVEDRA"],
-                ['value' => "26", "nombre" => "LA RIOJA"],
-                ['value' => "37", "nombre" => "SALAMANCA"],
-                ['value' => "38", "nombre" => "TENERIFE"],
-                ['value' => "40", "nombre" => "SEGOVIA"],
-                ['value' => "41", "nombre" => "SEVILLA"],
-                ['value' => "42", "nombre" => "SORIA"],
-                ['value' => "43", "nombre" => "TARRAGONA"],
-                ['value' => "44", "nombre" => "TERUEL"],
-                ['value' => "45", "nombre" => "TOLEDO"],
-                ['value' => "46", "nombre" => "VALENCIA"],
-                ['value' => "47", "nombre" => "VALLADOLID"],
-                ['value' => "48", "nombre" => "VIZCAYA"],
-                ['value' => "49", "nombre" => "ZAMORA"],
-                ['value' => "50", "nombre" => "ZARAGOZA"]
-            ];
+                        ['value' => "01", "nombre" => "ALAVA"],
+                        ['value' => "02", "nombre" => "ALBACETE"],
+                        ['value' => "03", "nombre" => "ALICANTE"],
+                        ['value' => "04", "nombre" => "ALMERIA"],
+                        ['value' => "33", "nombre" => "ASTURIAS"],
+                        ['value' => "05", "nombre" => "AVILA"],
+                        ['value' => "06", "nombre" => "BADAJOZ"],
+                        ['value' => "08", "nombre" => "BARCELONA"],
+                        ['value' => "09", "nombre" => "BURGOS"],
+                        ['value' => "10", "nombre" => "CACERES"],
+                        ['value' => "11", "nombre" => "CADIZ"],
+                        ['value' => "39", "nombre" => "CANTABRIA"],
+                        ['value' => "12", "nombre" => "CASTELLON"],
+                        ['value' => "51", "nombre" => "CEUTA"],
+                        ['value' => "13", "nombre" => "CIUDAD REAL"],
+                        ['value' => "14", "nombre" => "CORDOBA"],
+                        ['value' => "15", "nombre" => "CORUÑA"],
+                        ['value' => "16", "nombre" => "CUENCA"],
+                        ['value' => "17", "nombre" => "GIRONA"],
+                        ['value' => "18", "nombre" => "GRANADA"],
+                        ['value' => "19", "nombre" => "GUADALAJARA"],
+                        ['value' => "20", "nombre" => "GUIPUZKOA"],
+                        ['value' => "21", "nombre" => "HUELVA"],
+                        ['value' => "22", "nombre" => "HUESCA"],
+                        ['value' => "23", "nombre" => "JAEN"],
+                        ['value' => "24", "nombre" => "LEON"],
+                        ['value' => "25", "nombre" => "LLEIDA"],
+                        ['value' => "27", "nombre" => "LUGO"],
+                        ['value' => "28", "nombre" => "MADRID"],
+                        ['value' => "29", "nombre" => "MALAGA"],
+                        ['value' => "52", "nombre" => "MELILLA"],
+                        ['value' => "30", "nombre" => "MURCIA"],
+                        ['value' => "31", "nombre" => "NAVARRA"],
+                        ['value' => "32", "nombre" => "OURENSE"],
+                        ['value' => "34", "nombre" => "PALENCIA"],
+                        ['value' => "35", "nombre" => "LAS PALMAS"],
+                        ['value' => "36", "nombre" => "PONTEVEDRA"],
+                        ['value' => "26", "nombre" => "LA RIOJA"],
+                        ['value' => "37", "nombre" => "SALAMANCA"],
+                        ['value' => "38", "nombre" => "TENERIFE"],
+                        ['value' => "40", "nombre" => "SEGOVIA"],
+                        ['value' => "41", "nombre" => "SEVILLA"],
+                        ['value' => "42", "nombre" => "SORIA"],
+                        ['value' => "43", "nombre" => "TARRAGONA"],
+                        ['value' => "44", "nombre" => "TERUEL"],
+                        ['value' => "45", "nombre" => "TOLEDO"],
+                        ['value' => "46", "nombre" => "VALENCIA"],
+                        ['value' => "47", "nombre" => "VALLADOLID"],
+                        ['value' => "48", "nombre" => "VIZCAYA"],
+                        ['value' => "49", "nombre" => "ZAMORA"],
+                        ['value' => "50", "nombre" => "ZARAGOZA"]
+                    ];
                                 
                     ?>
                     <label for="buscarInput"><p>Código Provincia*<a href="https://www.el-tiempo.net/api" target="_blank"></a></p></label>
@@ -69,10 +68,10 @@
                         <option value="">Seleccione...</option>
                         <?php foreach($opciones as $key => $opcion){ ?>
                             <?php if($opcion['value'] == $_SESSION['nombreProvincia']=$_REQUEST['buscarInput']){ ?>
-                            <option selected value="<?= $opcion['value'] ?>"><?= $opcion['nombre'] ?></option>
-                        <?php }else{ ?>
-                            <option value="<?= $opcion['value'] ?>"><?= $opcion['nombre'] ?></option>
-                        <?php } ?>
+                                <option selected value="<?= $opcion['value'] ?>"><?= $opcion['nombre'] ?></option>
+                            <?php }else{ ?>
+                                <option value="<?= $opcion['value'] ?>"><?= $opcion['nombre'] ?></option>
+                            <?php } ?>
                         <?php } ?>
                     </select>
                     <p><?php echo($aErrores['eBuscarInput']!=null ? "<span style='color:red'>".$aErrores['eBuscarInput']."</span>" : null); ?></p>
